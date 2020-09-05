944d028aee476bd58130b6fea3c12bc843c7b530
ntiany <57062345+ntiany@users.noreply.github.com>
2020-09-05 15:29:17 +0200

    solved

--- ThreadExercise/Program.cs
+++ ThreadExercise/Program.cs
@@ -1,39 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 
 namespace ThreadExercise
 {
     class Program
     {
-        private List<Stopwatch> _stopwatches = new List<Stopwatch>();
-
         static void Main(string[] args)
         {
-            while (true)
+            var list = new List<ThreadKeeper>();
+            var running = true;
+
+            while (running)
             {
                 Console.WriteLine("Command?");
                 var answer = Console.ReadLine();
-                var answers = answer.Split();
+                var answers = answer?.Split();
 
+                if (answers != null && answers.Length == 2)
+                {
+                    var name = answers[1];
+                    var command = answers[0];
+                    var thread = list.FirstOrDefault(x => x.Name == name);
 
-
+                    if (thread == null && command == "start")
+                    {
+                        list.Add(new ThreadKeeper(name));
+                        Console.WriteLine($"Thread {name} added");
+                    }
+                    else if(thread != null && command == "stop")
+                    {
+                        thread.StopThread();
+                        list.Remove(thread);
+                        Console.WriteLine($"Thread {name} removed");
+                    }
+                    else if (thread != null && command == "show")
+                    {
+                        Console.WriteLine("Current threads:");
+                        Console.WriteLine($"Thread {thread.Thread.Name} Id: {thread.Thread.ManagedThreadId} is executing for {thread.Watch.ElapsedMilliseconds} ms");
+                    }
+                }
+                else if (answer == "show")
+                {
+                    list.ForEach(thread => Console.WriteLine($"Thread {thread.Thread.Name} Id: {thread.Thread.ManagedThreadId} is executing for {thread.Watch.ElapsedMilliseconds} ms"));
+                }
+                else if (answer == "exit")
+                {
+                    running = false;
+                }
+                else
+                {
+                    Console.WriteLine("Unknown command");
+                }
             }
-        }
-
-        public Thread CreateThread(string name)
-        {
-            var threadStart = new ThreadStart(AddNewStopwatch);
-            var thread = new Thread(threadStart);
-            return null;
-        }
-
-        public void AddNewStopwatch()
-        {
-            var stopwatch = new Stopwatch();
-            _stopwatches.Add(stopwatch);
-            stopwatch.Start();
         }
     }
 }
---
+++ ThreadExercise/ThreadKeeper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace ThreadExercise
+{
+    public class ThreadKeeper
+    {
+        public Stopwatch Watch { get; private set; }
+
+        public Thread Thread { get; private set; }
+
+        public string Name { get; private set; }
+
+        private bool _run = true;
+
+        public ThreadKeeper(string name)
+        {
+            Watch = new Stopwatch();
+            Thread = CreateThread(name);
+            StartThread();
+        }
+
+        public Thread CreateThread(string name)
+        {
+            var threadStart = new ThreadStart(DoNothing);
+            var thread = new Thread(threadStart);
+            Name = name;
+            thread.Name = name;
+            return thread;
+        }
+
+        public void DoNothing()
+        {
+            while (true)
+            {
+
+            }
+        }
+
+        public void StartThread()
+        {
+            Watch.Start();
+            Thread.Start();
+        }
+
+        public void StopThread()
+        {
+            Watch.Stop();
+            _run = false;
+        }
+
+    }
+}
